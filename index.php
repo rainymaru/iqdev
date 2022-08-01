0f633e8f5fa383478b053b307eddca45d88f1472
rainymaru <zaliatov.artyom@yandex.ru>
2022-08-01 09:09:28 +0500

    Добавил библиотеки в assets

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>IQ Dev Backend</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="assets/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+  <link rel="stylesheet" href="assets/  jquery-ui.css">
+  <script src="assets/jquery-1.12.4.js"></script>
+  <script src="assets/jquery-ui.js"></script>
+  <script src="assets/jquery.validate.min.js"></script>
 </head>
 <body>
   <div class="wrapper">
